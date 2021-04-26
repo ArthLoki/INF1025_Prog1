13bac81f811753243aab6af34352a0ad00f76899
Loki <80011623+ArthLoki@users.noreply.github.com>
2021-04-26 00:30:39 -0300

    create exercicio7.py

---
+++ Lista 7 - Recursão/Exercicio7.py
@@ -0,0 +1,28 @@
+# Questao 7
+
+def mudaBase(n: int, base: int) -> int:
+
+    # Casos base
+    if n == 0:
+        return 0
+    elif base == 10:
+        return n
+    
+    # Caso recursivo
+    resto = n % base
+    quociente = n // base
+    
+    return resto + mudaBase(quociente, base) * 10
+        
+
+def main_exercicio7():
+  
+    n = int(input("Digite um número na base 10: "))
+    base = int(input("Digite a base para o numero n ser convertido: "))
+    nBaseNova = mudaBase(n,base)
+    
+    print("Mudança de base: n = %d na base 10 ----> n = %d na base %d\n"%(n, nBaseNova, base))
+
+    return
+  
+  main_exercicio7()
